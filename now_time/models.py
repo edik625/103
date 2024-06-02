2e3b2b424afba0e247d80aa52db391abf2911500
edik625 <ednari.turmanidze@gmail.com>
2024-06-02 14:46:27 +0400

    creating my own times service

---
+++ now_time/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Times(models.Model):
+    currentTime = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.currentTime
+    
